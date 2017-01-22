89774b53bf140f75c409cc6c1025f7f20155bfa9
Lee Robb <lrobb@wdtinc.com>
2017-01-22 09:50:55 -0600

    modified to work with initial data

--- src/main/java/com/wdtinc/wdtnetcdfexample/Main.java
+++ src/main/java/com/wdtinc/wdtnetcdfexample/Main.java
@@ -10,31 +10,37 @@ public class Main {
 
     public static void main(String args[]) throws Exception {
 
-        String filename = "turb.nc";
-        NetcdfFile dataFile = NetcdfFile.open(filename, null);
+        String fileName = "SkyWise-Global7km-GFSAviation_20170120_1200.nc";
+        NetcdfFile dataFile = NetcdfFile.open(fileName, null);
 
         // print out the file structure
         System.out.println(dataFile.toString());
 
         // read the lat/lon and temperature variables
-        Variable lats = dataFile.findVariable("latitude");
-        Variable lons = dataFile.findVariable("longitude");
-        Variable turbs = dataFile.findVariable("turb");
-        Variable edrs = dataFile.findVariable("edr");
+        Variable latVar = dataFile.findVariable("latitude");
+        Variable lonVar = dataFile.findVariable("longitude");
+        Variable turbulenceVar = dataFile.findVariable("turbulence");
+        Variable edrVar = dataFile.findVariable("EDR");
 
         // Read the units for the temperature variable
-        String turbUnits = turbs.findAttributeIgnoreCase("units").getStringValue();
-        System.out.println("units: " + turbUnits);
-
-        String edrUnits = edrs.findAttributeIgnoreCase("units").getStringValue();
-        System.out.println("units: " + edrUnits);
+        System.out.println("units: " + turbulenceVar.findAttributeIgnoreCase("units"));
+        System.out.println("units: " + edrVar.findAttributeIgnoreCase("units"));
 
         // Read the valid time for this data
         Variable time = dataFile.findVariable("time");
-        System.out.println("data valid time: " + time.findAttributeIgnoreCase("string").getStringValue());
+        System.out.println("valid time: " + time.findAttributeIgnoreCase("string").getStringValue());
 
+        // if it's a forecast, get the forecast reference time
         Variable forecastTime = dataFile.findVariable("forecast_reference_time");
-        System.out.println("data valid time: " + forecastTime.findAttributeIgnoreCase("string").getStringValue());
+        System.out.println("forecast time: " + forecastTime.findAttributeIgnoreCase("string").getStringValue());
+
+        // Read the latitude and longitude coordinate variables into arrays
+        ArrayFloat.D2 lats = (ArrayFloat.D2) latVar.read();
+        ArrayFloat.D2 lons = (ArrayFloat.D2) lonVar.read();
+
+        // Read the turbulence & edr data
+        ArrayFloat.D2 turbulence = (ArrayFloat.D2) turbulenceVar.read();
+        ArrayFloat.D2 edr = (ArrayFloat.D2) edrVar.read();
 
         // dimensions of the lat/lon array
         List<Dimension> dimensions = dataFile.getDimensions();
@@ -44,20 +50,14 @@ public class Main {
         System.out.println("lon length: " + lonLength);
         System.out.println("lat length: " + latLength);
 
-        // Read the latitude and longitude coordinate variables into arrays
-        ArrayFloat.D2 latArray = (ArrayFloat.D2) lats.read();
-        ArrayFloat.D2 lonArray = (ArrayFloat.D2) lons.read();
-        
-        // Read the turbulence & edr data
-        ArrayFloat.D2 turbArray = (ArrayFloat.D2) turbs.read();
-        ArrayFloat.D2 edrArray = (ArrayFloat.D2) edrs.read();
-
+        // iterate through the arrays, do something with the data
         for (int lat = 0; lat < latLength; lat++) {
             for (int lon = 0; lon < lonLength; lon++) {
-                System.out.print(latArray.get(lat, lon) + ",");
-                System.out.print(lonArray.get(lat, lon) + ",");
-                System.out.print(turbArray.get(lat, lon) + ",");
-                System.out.println(edrArray.get(lat, lon));
+                // do something with the data
+                System.out.print(lats.get(lat, lon) + ",");
+                System.out.print(lons.get(lat, lon) + ",");
+                System.out.print(turbulence.get(lat, lon) + ",");
+                System.out.println(edr.get(lat, lon));
             }
         }
     }
